8b28eb474f2ef614cca5b9bd3d7ca27329ad1a14
Jéssica da Silva Oliveira <jessica.oliveira@redes.unb.br>
2020-08-09 16:47:10 -0300

    day 3

---
+++ scripts/addField.js
@@ -0,0 +1,20 @@
+﻿// Procurar o botão
+document.querySelector("#add-time")
+// Quando clicar no botaõ
+.addEventListener("click", cloneField)
+// Executar uma ação de
+function cloneField() {
+    // Duplicar os campos. Que campos?
+    const newFieldContainer = document.querySelector('.schedule-item').cloneNode(true)
+
+    //limpar os campos. Que campos?
+    const fields = newFieldsContainer.querySelectorAll('input')
+
+    fields.forEach(function(field){
+        field.value = ""
+    })
+
+    // Colocar na página: onde?
+    document.querySelector('#schedule-items').appendChild(newFieldContainer)
+
+}
